5335a820641cc35e45ef7e19b77f260bd5889b92
Arthur <arthur.cahen@radius.com>
2017-09-24 13:02:32 -0700

    Document first time set-up commands

---
+++ setup/a_to_z.sh
@@ -0,0 +1,10 @@
+# homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# pip
+conda easy_install pip
+# zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+chsh -s /usr/local/bin/zsh
+brew install zsh zsh-completions
+# tree
+brew install tree

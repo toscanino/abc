4d6c2707f73a355418dd373f43f023b493340a85
Arthur <arthurcahen@users.noreply.github.com>
2019-02-28 15:24:35 -0800

    Prezto config

--- setup/a_to_z.sh
+++ setup/a_to_z.sh
@@ -7,6 +7,12 @@ export PATH=~/miniconda3/bin:$PATH
 conda install pip
 # zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+# follow the steps (zsh config manager) https://github.com/sorin-ionescu/prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 chsh -s /usr/local/bin/zsh
 brew install zsh zsh-completions
 # tree

2648c00869a37ed4ae65bbffb39a9ab742fcaf4c
Arthur Cahen <arthurcahen@ArthurCahen-MBP2.local>
2019-03-04 11:33:14 -0800

    separate brew packages

--- setup/a_to_z.sh
+++ setup/a_to_z.sh
@@ -14,34 +14,3 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 chsh -s /usr/local/bin/zsh
-# set up subl to open sublime
-ln -sv "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sub
-brew install zsh zsh-completions
-# tree
-brew install tree
-# pipe viewer
-brew install pv
-# auto-jump
-brew install autojump
-# gpg encryption
-brew install gpg
-# fuzzy search with recursion
-brew install fzf
-# display files with highlighting
-brew install bat
-# alternative colorful ls
-brew install exa
-# better simpler find
-brew install fd
-# better grep
-brew install rp
-# interactive filter
-brew install peco
-# analyze disk space
-brew install ncdu
-# better curl
-brew install httpie
-# file selector
-brew install fpp
-# json player
-brew install gron 

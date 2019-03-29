5a041d47e5a8df6999b298399fce4c38445b9dbc
arthurcahen <arthur.cahen@gmail.com>
2019-03-29 14:09:59 -0700

    Additions

--- setup/a_to_z.sh
+++ setup/a_to_z.sh
@@ -14,3 +14,4 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 chsh -s /usr/local/bin/zsh
+git config --global alias.st status

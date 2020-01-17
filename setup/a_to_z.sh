61c0bfdad6213313bfdcfba3620c7f20689d588e
Arthur <arthurcahen@users.noreply.github.com>
2020-01-17 12:19:35 +0100

    tweaks

--- setup/a_to_z.sh
+++ setup/a_to_z.sh
@@ -1,12 +1,6 @@
 # homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install zsh
-# install conda
-brew cask install anaconda
-conda init
-export PATH=~/miniconda3/bin:$PATH
-# pip
-conda install pip
 # zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 # follow the steps (zsh config manager) https://github.com/sorin-ionescu/prezto
@@ -16,5 +10,13 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 chsh -s /usr/local/bin/zsh
+# git alias
 git config --global alias.st status
+# system I/O preference
 defaults write com.apple.screencapture location /Users/arthurcahen/Documents/screenshots
+# install conda
+brew cask install anaconda
+# not sure about the setup, been a pain, try this
+conda init
+# install java
+brew cask install java

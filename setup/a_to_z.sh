4413ea466d6429f7e8730d0cbf91bb0ce39bdae6
Arthur <arthurcahen@users.noreply.github.com>
2021-04-19 13:56:27 +0200

    clean up

--- setup/a_to_z.sh
+++ setup/a_to_z.sh
@@ -1,17 +1,8 @@
-# WORK IN PROGRESS
 # homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh
-# zsh
+# oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-# follow the steps (zsh config manager) https://github.com/sorin-ionescu/prezto
+# zsh config & bonus
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# choose shell
 chsh -s /usr/local/bin/zsh
-# git alias
-git config --global alias.st status
-# system I/O preference
-defaults write com.apple.screencapture location /Users/arthurcahen/Documents/screenshots
